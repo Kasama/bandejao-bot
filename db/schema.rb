a1de4f14201c332d64b4b4d7376ad35ca3c63743
Kasama <ralegro@bol.com.br>
2017-03-08 11:52:44 -0300

    separated development and production dbs

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170307010644) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "schedules", force: :cascade do |t|
     t.integer "user_id"
     t.integer "chat_id",  limit: 8, null: false
@@ -20,8 +23,7 @@ ActiveRecord::Schema.define(version: 20170307010644) do
     t.string  "command"
   end
 
-  create_table "users", id: false, force: :cascade do |t|
-    t.integer  "id"
+  create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
@@ -29,7 +31,8 @@ ActiveRecord::Schema.define(version: 20170307010644) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "users", ["id"], name: "index_users_on_id"
-  add_index "users", ["username"], name: "index_users_on_username"
+  add_index "users", ["id"], name: "index_users_on_id", using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
+  add_foreign_key "schedules", "users"
 end
