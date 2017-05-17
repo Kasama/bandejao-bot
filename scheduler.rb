7c3ab5500c5c5257e1b245282db6eddd90f072e6
Kasama <ralegro@bol.com.br>
2017-05-17 15:36:49 -0300

    added parallel test

--- scheduler.rb
+++ scheduler.rb
@@ -16,22 +16,21 @@ class Scheduler
       @scheduler.cron CONST::CRON_EXP[per] do
         puts "==================== REACHED SCHEDULE ========================"
         now = Time.now
-        bot.bot.api.send_message(
-          chat_id: CONST::MASTER_ID,
-          text: "Reached schedule at #{now}."
-        )
         all_schedules = Schedule.all.to_a
-        threads = (all_schedules.size / 10)
+        threads = (all_schedules.size / 5).to_i + 1
         if threads > CONST::MAX_THREADS
           threads = CONST::MAX_THREADS
         end
-        Schedule.all.each do |schedule|
-          # Parallel.each(all_schedules, in_threads: threads) do |schedule|
+        bot.bot.api.send_message(
+          chat_id: CONST::MASTER_ID,
+          text: "Reached schedule at #{now}. Allocating #{ all_schedules.size } messages in #{ threads } threads."
+        )
+        # Schedule.all.each do |schedule|
+        Parallel.each(all_schedules, in_threads: threads) do |schedule|
           begin
-            puts "Sending message to #{schedule.user_id} from thread" #{Parallel.worker_number}"
+            puts "Sending message to #{schedule.user_id} from thread #{Parallel.worker_number}"
             puts "============================================================"
-            message = build_message(schedule.user_id, schedule.chat_id)
-            bot.run_schedule message
+            bot.run_schedule build_message(schedule.user_id, schedule.chat_id)
           rescue => e
             puts "Could not Send message to #{schedule.user_id}, skipping"
             puts "============================================================"
