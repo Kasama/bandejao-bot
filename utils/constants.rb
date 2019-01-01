f49c76f28c783b93939e809ac2b4177ee2bffe9c
Gurulhu <lucasag@hotmail.com.br>
2019-01-01 19:55:23 -0200

    Fixed a minor bug in constants.rb as requested by @Kasama

--- utils/constants.rb
+++ utils/constants.rb
@@ -132,7 +132,7 @@ module CONST
     unsubscribe: /unsubscribe|des(?:in|en?)screver|desativar/i,
     config: /\bconfig(?:ura(?:r|(?:c|ç)(?:o|õ)es)|\b)|\bpref(?:er(?:e|ê)nc(?:es|ias)|\b)/i,
     feedback: /\b(?:feedback|report)\b/i,
-    alguem: /\balgu(?:e|é)m\b/i
+    alguem: /\balgu(?:e|é)m\b/i,
     papoco: /\bacende\b/i
   }.freeze
 
